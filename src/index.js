a83d7b9436aed6c79ffbf5e62f8d7a71203251f8
comfortibok <ibokcomfort93@gmail.com>
2024-12-02 15:44:15 +0100

    Updated my weather app with forecast API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 function searchTemperature(response) {
-    let cityElement = document.getElementById("city");
-    cityElement.innerHTML = response.data.city;
+  let cityElement = document.getElementById("city");
+  cityElement.innerHTML = response.data.city;
 
-    let date = new Date(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
 
-    let timeElement = document.getElementById("time");
-    timeElement.innerHTML = formatDate(date);
+  let timeElement = document.getElementById("time");
+  timeElement.innerHTML = formatDate(date);
 
-    let descriptionElement = document.getElementById("description");
-    descriptionElement.innerHTML = response.data.condition.description;
+  let descriptionElement = document.getElementById("description");
+  descriptionElement.innerHTML = response.data.condition.description;
 
-    let humidityElement = document.getElementById("humidity");
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let humidityElement = document.getElementById("humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
-    let windSpeedElement = document.getElementById("windspeed");
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let windSpeedElement = document.getElementById("windspeed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
   let temperatureElement = document.getElementById("temperature");
   let temperature = Math.round(response.data.temperature.current);
@@ -53,6 +53,41 @@ function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.getElementById("search-input");
   searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+let apiKey = "5of280a2b12980f017dcf8ff8fda334t";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios(apiUrl).then(displayForecastInfo);
+
+function displayForecastInfo(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div>
+            <div class="weather-forecast-day">${formatDay(day.time)}</div>
+            <div class="weather-forecast-symbol">
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+            </div>
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong> ${Math.round(day.temperature.minimum)}°
+            </div>
+          </div>
+       `;
+    }
+  });
+  let forecastElement = document.getElementById("forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let form = document.getElementById("form");
